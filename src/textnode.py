0aad454fd4dad1628c73efdeb95997b08ae561f5
Pablowski <pabloleo1528@gmail.com>
2026-04-11 14:56:03 -0700

    A: implemented a bit of textnodes and testing codes

---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.type = text_type
+        self.url = url
+    
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):
+            return False
+        return (
+            self.text == other.text 
+            and self.type == other.type
+            and self.url == other.url
+        )
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.type}, {self.url})"
+
+class TextType(Enum):
+    TEXT = 1
+    BOLD = 2
+    ITALIC = 3
+    CODE = 4
+    LINK = 5
+    IMAGE = 6   
